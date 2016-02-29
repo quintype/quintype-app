1a1b91cc6fbb6d446f092f798465769afd0830b2
chopraapooja <all.is.well.chopra@gmail.com>
2016-02-29 14:04:46 +0530

    jmx

--- resources/leiningen/new/quintype_app/project.clj
+++ resources/leiningen/new/quintype_app/project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [ring "1.4.0"]
+                 [org.eclipse.jetty/jetty-jmx "9.2.10.v20150310"]
                  [cider/cider-nrepl "0.10.2"]
                  [clj-log4j2 "0.2.0"]]
   :main ^:skip-aot {{namespace}}.core
